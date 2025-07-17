25b1008e3205b6d3b6bf6d9f8d961f29454ee54d
Elayne Fernandes <elaynefernandesoliveira@gmail.com>
2025-07-17 20:43:43 -0300

    static e templates

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path 
-from . import viwes
+from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('sobre/', views.sobre, name="sobre"),
+    path('elenco/', views.elenco, name="elenco"),
+    path('diario/', views.diario, name="diario"),
 ]
